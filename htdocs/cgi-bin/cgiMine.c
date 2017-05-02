428077658f8257f8a7fae381a337d76c001d2249
wusanzi <wusanzi@askat.net>
2017-05-02 17:01:14 +0800

    添加C实现的cgi-bin

---
+++ htdocs/cgi-bin/cgiMine.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#define	MAXLINE	 8192  /* max text line length */
+
+int main(int argc, char *argv[]) {
+    char *buf,*p;
+    int length=0;
+    char data[MAXLINE];
+    char content[MAXLINE] = {0};
+    //printf("argv[1] = %s\n", argv[1]);
+
+    if ((buf = getenv("CONTENT_LENGTH")) != NULL)
+    {
+    	length=atol(buf);
+    }
+    else
+    {
+	printf("getenv == NULL\n");
+    }
+
+    //printf("length = %d\n", length);
+
+    sprintf(content, "%s\r\n", "<HTML>");
+
+    printf("<HTML>\r\n");
+    printf("<TITLE>Index</TITLE>\r\n");
+    printf("<BODY>\r\n");
+    printf("<P>Welcome to J. David's webserver.\r\n");
+    printf("<H1>CGI_MINE demo\r\n");
+    printf("<FORM ACTION=\"color.cgi\" METHOD=\"POST\">\r\n");
+    printf("Enter a color: <INPUT TYPE=\"text\" NAME=\"color\">\r\n");
+    printf("<INPUT TYPE=\"submit\">\r\n");
+    printf("</FORM>\r\n");
+    printf("</BODY>\r\n");
+    printf("</HTML>\r\n");
+
+    fflush(stdout);
+
+    return 0; 
+    //exit(0);
+ 
+   #if 0 
+    p=fgets(data,length+1,stdin);
+    if(p==NULL)
+    	sprintf(content, "Something is wrong\r\n");
+    else	
+	sprintf(content, "Info:%s\r\n",data);
+
+    printf("Content-length: %d\r\n", strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+    exit(0);
+   #endif 
+
+}
